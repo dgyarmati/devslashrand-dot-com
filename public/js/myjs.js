d58cbfbfacb250c1f7caa2d2f7fdb5549953e448
dgyarmati <dpolya.w@gmail.com>
2020-07-28 13:44:30 +0200

    removed unused script

--- public/js/myjs.js
+++ public/js/myjs.js
@@ -18,56 +18,7 @@ $(function() {
 
 // Slick FX
 $(document).ready(function(){
-    $("#showmailbox").click(function(){
-        $("#mailbox").slideToggle();
-    });
-    $("#showheaderhidden1").click(function(){
-        $("#headerhidden1").slideToggle();
-    });
     $("#showsearch").click(function(){
         $("#search").slideToggle();
     });
-  {% for title in site.data.cv.employment %}
-    $("#showEmployment{{ forloop.index }}").click(function(){
-        $("#employment{{ forloop.index }}").slideToggle();
-    });
-  {% endfor %}
-  {% for title in site.data.cv.projects %}
-    $("#showProject{{ forloop.index }}").click(function(){
-        $("#project{{ forloop.index }}").slideToggle();
-    });
-    $("#showMinorProject{{ forloop.index }}").click(function(){
-        $("#minorProject{{ forloop.index }}").slideToggle();
-    });
-  {% endfor %}
-  {% for title in site.data.cv.creative %}
-    $("#showCreative{{ forloop.index }}").click(function(){
-        $("#creative{{ forloop.index }}").slideToggle();
-    });
-  {% endfor %}
-  {% for title in site.data.cv.courses %}
-    $("#showCourse{{ forloop.index }}").click(function(){
-        $("#course{{ forloop.index }}").slideToggle();
-    });
-  {% endfor %}
-  {% for title in site.data.cv.awards %}
-    $("#showAward{{ forloop.index }}").click(function(){
-        $("#award{{ forloop.index }}").slideToggle();
-    });
-  {% endfor %}
-  {% for title in site.data.research.publications %}
-    $("#showPublications{{ forloop.index }}").click(function(){
-        $("#publications{{ forloop.index }}").slideToggle();
-    });
-  {% endfor %}
-  {% for title in site.data.research.public %}
-    $("#showPublic{{ forloop.index }}").click(function(){
-        $("#public{{ forloop.index }}").slideToggle();
-    });
-  {% endfor %}
-  {% for title in site.data.research.projects %}
-    $("#showProjects{{ forloop.index }}").click(function(){
-        $("#projects{{ forloop.index }}").slideToggle();
-    });
-  {% endfor %}
 });
